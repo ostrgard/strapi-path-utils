7e6c865d1a86ac2d1221fbdac66a94cc0f7261b1
Søren Østergaard <soestergaards@gmail.com>
2020-09-03 14:43:21 +0200

    fix main exports doh!

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const generateController = require("generateController").default;
-const generateModel = require("generateModel").default;
+const generateController = require("./src/generateController").default;
+const generateModel = require("./src/generateModel").default;
 
 module.exports = {
   generateController,
